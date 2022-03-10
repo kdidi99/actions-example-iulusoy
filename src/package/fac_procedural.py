16a03b2b1e085dacc59e6b64633842be1f55f683
Kieran Didi <58345129+kdidi99@users.noreply.github.com>
2022-03-10 10:36:10 +0100

    corrected one of two deliberate mistakes

--- src/package/fac_procedural.py
+++ src/package/fac_procedural.py
@@ -4,7 +4,7 @@ import argparse
 def parse_command_line() -> int:
     """Function to parse user input. User input is an integer.
     :Returns: n (input integer)"""
-    parser = argparse.ArgumentParser(,
+    parser = argparse.ArgumentParser(
         description="Program to calculate \
         factorial of n. Check -h or --help for options. \
         Usage: ./main.py -n 4"
